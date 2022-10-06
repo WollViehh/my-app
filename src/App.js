b8d25f0881798308b35aea663ca1fa7e4862a82c
WollViehh <wolfgang.fritz@catenda.no>
2022-10-06 08:01:36 +0200

    second Time

---
+++ src/App.js
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div ClassName="cal-grid">
+      <div ClassName="outpud">
+        <div ClassName="privius-operant"></div>
+        <div ClassName="current-operant"></div>
+      </div>
+
+      <button ClassName="span-tow">AC</button>
+      <button>DEL</button>
+      <button>/</button>
+    </div>
+
+  ) 
+
+}
+
+export default App;
